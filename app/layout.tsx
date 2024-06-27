50ab77ed801b084e92d4d7b2e4d42a9b301dcd2b
DraconDev <dracsharp@gmail.com>
2024-06-27 20:42:23 +0100

    add menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { NavigationMenu } from "@radix-ui/react-navigation-menu";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <NavigationMenu />
+                {children}
+            </body>
         </html>
     );
 }
