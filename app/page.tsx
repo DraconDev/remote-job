92170e86e83005417a3eb22d3957bf95742b7b75
DraconDev <dracsharp@gmail.com>
2024-07-05 22:54:34 +0100

    save

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import JobCards from "@/components/JobCards/JobCards";
+import { Tiptap } from "@/components/TipTap/Description";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
         <main className=" w-full p-2">
-            hello
+            <Tiptap />
             <Button variant="default" size="default">
                 Elemen
             </Button>
