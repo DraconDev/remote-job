1f2bd959df254369cd334a3ddb022a340b834439
DraconDev <dracsharp@gmail.com>
2024-07-05 17:35:02 +0100

    add types

--- app/postjob/page.tsx
+++ app/postjob/page.tsx
@@ -1,6 +1,30 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
 type Props = {};
 
 const page = (props: Props) => {
+    // supabase cookieStore
+    const cookieStore = cookies();
+
+    // add job_post to supabase
+
+    const supabase = createClient(cookieStore);
+
+    // create job_post in supabase
+    function createJobPost(jobPost: any) {
+        return supabase.from("jobPost").insert(jobPost);
+        // const { data: jobPost } = supabase.from("jobPost").insert({
+        //     created_at: new Date(),
+        //     salary_min: 10000,
+        //     salary_max: 20000,
+        //     tags: ["tag1", "tag2"],
+        //     description: "description",
+        //     location: "location",
+        //     company_name: "company_name",
+        //     job_title: "job_title",
+    }
+
     return <div>page</div>;
 };
 
