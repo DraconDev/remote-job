eaabf3e9463b51ec2e33836ec8c164ec66ea0894
DraconDev <dracsharp@gmail.com>
2024-07-08 20:36:39 +0100

    fix bug

--- components/PostJobForm/PostJobForm.tsx
+++ components/PostJobForm/PostJobForm.tsx
@@ -1,4 +1,3 @@
-import { Database } from "@/types/supabase";
 import { Button } from "../ui/button";
 import Input from "../ui/Input";
 import Description from "@/app/Quill/Description";
@@ -16,28 +15,6 @@ const PostJobForm = (props: Props) => {
         const tag3 = JSON.parse(formData.get("tag3") as string) || "";
         const tag4 = JSON.parse(formData.get("tag4") as string) || "";
         const tag5 = JSON.parse(formData.get("tag5") as string) || "";
-
-        const rawFormData: Database["public"]["Tables"]["job_post"]["Insert"] =
-            {
-                salary_min: formData.get("salary_min")
-                    ? parseInt(formData.get("salary_min") as string)
-                    : null,
-                salary_max: formData.get("salary_max")
-                    ? parseInt(formData.get("salary_max") as string)
-                    : null,
-                tags:
-                    [
-                        tag1 && tag1,
-                        tag2 && tag2,
-                        tag3 && tag3,
-                        tag4 && tag4,
-                        tag5 && tag5,
-                    ] || [],
-                description: (formData.get("description") as string) || "",
-                location: (formData.get("location") as string) || "",
-                company_name: (formData.get("company_name") as string) || "",
-                job_title: (formData.get("job_title") as string) || "",
-            };
     }
 
     // add outline when active
