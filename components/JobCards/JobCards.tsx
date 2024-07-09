0443c6ea6e2b1f5e5a5ee62e690d35b0c3233d53
DraconDev <dracsharp@gmail.com>
2024-07-09 17:45:11 +0100

    add vitest

--- components/JobCards/JobCards.tsx
+++ components/JobCards/JobCards.tsx
@@ -13,21 +13,29 @@ const JobCards = (props: Props) => {
 
     useEffect(() => {
         if (!data) {
-            getJobs().then((jobs) => setData(jobs));
+            getJobs()
+                .then((jobs) => {
+                    setData(jobs);
+                })
+                .catch((error) => {
+                    console.error("Failed to fetch jobs:", error);
+                    // Optionally, update the state to indicate an error occurred.
+                });
         }
     }, []);
 
-    // return <div>{data && JSON.stringify(data)}</div>;
     return (
         <div>
-            {data &&
+            {data ? (
                 data.map(
                     (job: Database["public"]["Tables"]["job_post"]["Row"]) => (
                         <JobCard key={job.id} {...job} />
                     )
-                )}
+                )
+            ) : (
+                <p>Loading jobs...</p>
+            )}
         </div>
     );
 };
-
 export default JobCards;
--- components/PostJobForm/PostJobForm.tsx
+++ components/PostJobForm/PostJobForm.tsx
@@ -1,47 +1,13 @@
 "use client";
-import { Database } from "@/types/supabase";
 import { Button } from "../ui/button";
 import Input from "../ui/Input";
 import { useState } from "react";
-import { createJobPost } from "@/utils/supabase/actions";
+import { createInvoice } from "@/utils/supabase/actions";
 
 type Props = {};
 
 const PostJobForm = (props: Props) => {
     const [content, setContent] = useState();
-    async function createInvoice(formData: FormData) {
-        "use server";
-
-        const tag1 = JSON.parse(formData.get("tag1") as string) || "";
-        const tag2 = JSON.parse(formData.get("tag2") as string) || "";
-        const tag3 = JSON.parse(formData.get("tag3") as string) || "";
-        const tag4 = JSON.parse(formData.get("tag4") as string) || "";
-        const tag5 = JSON.parse(formData.get("tag5") as string) || "";
-
-        const rawFormData: Database["public"]["Tables"]["job_post"]["Insert"] =
-            {
-                salary_min: formData.get("salary_min")
-                    ? parseInt(formData.get("salary_min") as string)
-                    : null,
-                salary_max: formData.get("salary_max")
-                    ? parseInt(formData.get("salary_max") as string)
-                    : null,
-                tags:
-                    [
-                        tag1 && tag1,
-                        tag2 && tag2,
-                        tag3 && tag3,
-                        tag4 && tag4,
-                        tag5 && tag5,
-                    ] || [],
-                description: (formData.get("description") as string) || "",
-                location: (formData.get("location") as string) || "",
-                company_name: (formData.get("company_name") as string) || "",
-                job_title: (formData.get("job_title") as string) || "",
-                apply_link: (formData.get("apply_link") as string) || "",
-            };
-        createJobPost(rawFormData);
-    }
 
     // add outline when active
     const buttonStyles = "font-bold py-1 px-2 ";
