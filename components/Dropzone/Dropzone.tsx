3ebd60edf905c7affbef98fc73677efb14a48aab
DraconDev <dracsharp@gmail.com>
2024-07-22 03:37:00 +0100

    upd dropzone

--- components/Dropzone/Dropzone.tsx
+++ components/Dropzone/Dropzone.tsx
@@ -11,7 +11,8 @@ function MyDropZone() {
     const onDrop = useCallback((acceptedFiles: File[]) => {
         // Update the state to the first selected image file
         const file = acceptedFiles[0];
-        if (file.size > 1024 * 64 * 64) { // 64x64px image in bytes
+        if (file.size > 1024 * 64 * 64) {
+            // 64x64px image in bytes
             console.log("File size is too large");
             return;
         }
@@ -38,20 +39,20 @@ function MyDropZone() {
             <div className="">64x64px large avatar in png or jpeg format</div>
             <div
                 {...getRootProps()}
-                className="bg-red-400 w-[64px] h-[64px]  hover:cursor-pointer rounded-md"
+                className="border-2 border-black w-[64px] h-[64px]  hover:cursor-pointer  rounded-full"
             >
                 <input {...getInputProps()} />
                 {selectedImage ? (
                     <Image
                         src={selectedImage}
                         alt="Selected"
-                        className="w-full h-full object-cover"
+                        className="w-full h-full object-cover rounded-full"
                         width="64"
                         height="64"
                     />
                 ) : (
                     <div className="justify-center items-center flex w-full h-full ">
-                        <MdUpload className="w-16 h-16" />
+                        <MdUpload className="w-12 h-12" />
                     </div>
                 )}
             </div>
