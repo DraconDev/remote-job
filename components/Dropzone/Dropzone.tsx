4db7dea04d0ef81da88169636fba9344ca5a7c5e
DraconDev <dracsharp@gmail.com>
2025-05-08 21:38:02 +0100

    fix(dropzone): make dropzone width full width The dropzone s width was changed from a fixed 150px to full width.

--- components/Dropzone/Dropzone.tsx
+++ components/Dropzone/Dropzone.tsx
@@ -49,7 +49,7 @@ function MyDropZone({
 
       <div
         {...getRootProps()}
-        className="border-2 border-dashed border-gray-500 w-[150px] h-[150px] flex items-center justify-center hover:cursor-pointer rounded-lg p-6"
+        className="border-2 border-dashed border-gray-500 w-full h-[150px] flex items-center justify-center hover:cursor-pointer rounded-lg p-6"
       >
         <input {...getInputProps()} type="file" accept="image/*" />
         {selectedImage ? (
