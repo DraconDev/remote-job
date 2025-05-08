feb4aa750c99464724acdf625ea59131cd54be67
DraconDev <dracsharp@gmail.com>
2025-05-08 21:35:46 +0100

    feat: update 1 files (components/PostJobForm/PostJobSection.tsx) at 20:35

--- components/PostJobForm/PostJobSection.tsx
+++ components/PostJobForm/PostJobSection.tsx
@@ -8,8 +8,8 @@ const PostJobSection = (props: Props) => {
   const buttonStyles = "p-1 font-bold";
   const paragraphStyles = "font-bold text-xl border-b-2 border-accent";
   return (
-    <div className={`${buttonStyles} flex flex-col gap-1 pb-4`}>
-      <p className={paragraphStyles}>{props.title}</p>
+    <div className="w-full flex flex-col gap-3 pb-4">
+      <p className="font-bold text-xl border-b-2 border-accent text-center">{props.title}</p>
       {props?.children}
     </div>
   );
