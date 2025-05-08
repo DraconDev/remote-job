ea222a0c2ccc6fa3edb05a9ad979e31c9c70f25d
DraconDev <dracsharp@gmail.com>
2025-05-08 21:34:59 +0100

    feat: update 1 files (components/PostJobForm/PostJobSection.tsx) at 20:34

--- components/PostJobForm/PostJobSection.tsx
+++ components/PostJobForm/PostJobSection.tsx
@@ -1,18 +1,18 @@
 type Props = {
-    extra?: string;
-    title: string;
-    children?: React.ReactNode;
+  extra?: string;
+  title: string;
+  children?: React.ReactNode;
 };
 
 const PostJobSection = (props: Props) => {
-    const buttonStyles = "p-1 font-bold";
-    const paragraphStyles = "font-bold text-xl border-b-2 border-accent";
-    return (
-        <div className={`${buttonStyles} flex flex-col gap-3`}>
-            <p className={paragraphStyles}>{props.title}</p>
-            {props?.children}
-        </div>
-    );
+  const buttonStyles = "p-1 font-bold";
+  const paragraphStyles = "font-bold text-xl border-b-2 border-accent";
+  return (
+    <div className={`${buttonStyles} flex flex-col gap-1 pb-4`}>
+      <p className={paragraphStyles}>{props.title}</p>
+      {props?.children}
+    </div>
+  );
 };
 
 export default PostJobSection;
