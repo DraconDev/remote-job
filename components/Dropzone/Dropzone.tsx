63551d5a490bad2e9072a8ac771fc389ccaaf16c
DraconDev <dracsharp@gmail.com>
2025-05-08 21:37:53 +0100

    feat: update 1 files (components/Dropzone/Dropzone.tsx) at 20:37

--- components/Dropzone/Dropzone.tsx
+++ components/Dropzone/Dropzone.tsx
@@ -6,67 +6,71 @@ import { useDropzone } from "react-dropzone";
 import Image from "next/image";
 
 function MyDropZone({
-    setSelectedFile,
+  setSelectedFile,
 }: {
-    setSelectedFile: (file: File | null) => void;
+  setSelectedFile: (file: File | null) => void;
 }) {
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-    const onDrop = useCallback((acceptedFiles: File[]) => {
-        // Update the state to the first selected image file
-        console.log(acceptedFiles);
-        const file = acceptedFiles[0];
-        if (file.size > 10000) {
-            // 64x64px image in bytes
-            console.log("File size is too large");
-            return;
-        }
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Update the state to the first selected image file
+    console.log(acceptedFiles);
+    const file = acceptedFiles[0];
+    if (file.size > 10000) {
+      // 64x64px image in bytes
+      console.log("File size is too large");
+      return;
+    }
 
-        setSelectedFile(file);
-        const reader = new FileReader();
+    setSelectedFile(file);
+    const reader = new FileReader();
 
-        reader.onload = (e) => {
-            if (e?.target?.result) {
-                setSelectedImage(e.target.result as string);
-            }
-        };
+    reader.onload = (e) => {
+      if (e?.target?.result) {
+        setSelectedImage(e.target.result as string);
+      }
+    };
 
-        reader.readAsDataURL(file);
-    }, []);
+    reader.readAsDataURL(file);
+  }, []);
 
-    const { getRootProps, getInputProps } = useDropzone({
-        onDrop,
-        accept: {
-            "image/jpeg": [],
-            "image/png": [],
-        },
-    });
-    return (
-        <>
-            <div className="text-center text-sm text-gray-600 mb-2">Upload your company logo (max 64x64px, PNG or JPEG)</div>
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      "image/jpeg": [],
+      "image/png": [],
+    },
+  });
+  return (
+    <>
+      <div className="text-center text-sm text-gray-600 mb-2">
+        Upload your company logo (max 64x64px, PNG or JPEG)
+      </div>
 
-            <div
-                {...getRootProps()}
-                className="border-2 border-dashed border-gray-500 w-full h-[150px] flex items-center justify-center hover:cursor-pointer rounded-lg p-6"
-            >
-                <input {...getInputProps()} type="file" accept="image/*" />
-                {selectedImage ? (
-                    <Image
-                        src={selectedImage}
-                        alt="Selected"
-                        className="w-full h-full object-cover rounded-lg"
-                        width={150}
-                        height={150}
-                    />
-                ) : (
-                    <div className="flex flex-col items-center justify-center w-full h-full text-gray-500">
-                        <MdUpload className="w-8 h-8 mb-2" />
-                        <p className="text-sm text-center">Drag &apos;n&apos; drop or click to upload</p>
-                    </div>
-                )}
-            </div>
-        </>
-    );
+      <div
+        {...getRootProps()}
+        className="border-2 border-dashed border-gray-500 w-[150px] h-[150px] flex items-center justify-center hover:cursor-pointer rounded-lg p-6"
+      >
+        <input {...getInputProps()} type="file" accept="image/*" />
+        {selectedImage ? (
+          <Image
+            src={selectedImage}
+            alt="Selected"
+            className="w-full h-full object-cover rounded-lg"
+            width={150}
+            height={150}
+          />
+        ) : (
+          <div className="flex flex-col items-center justify-center w-full h-full text-gray-500">
+            <MdUpload className="w-8 h-8 mb-2" />
+            <p className="text-sm text-center">
+              Drag &apos;n&apos; drop or click to upload
+            </p>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default MyDropZone;
