fd09dce949a2eaa766c777446b70fc0f04b662ae
DraconDev <dracsharp@gmail.com>
2025-05-09 16:50:00 +0100

    fix(types): Add company_logo_public_url to JobPostWithCompanies type

--- types/custom.ts
+++ types/custom.ts
@@ -2,7 +2,9 @@ import { Database } from "./supabase";
 
 export type JobPostWithCompanies =
     Database["public"]["Tables"]["job_post"]["Row"] & {
-        companies: Database["public"]["Tables"]["companies"]["Row"];
+        companies: Database["public"]["Tables"]["companies"]["Row"] & {
+            company_logo_public_url?: string;
+        };
     };
 
 export type searchType = {
